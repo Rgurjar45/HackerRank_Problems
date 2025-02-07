cbf0a5afa71390fd80098edbaea564c89c9806ac
Rgurjar45 <rgujjar4567@gmail.com>
2025-02-07 14:41:23 +0530

    Add Largest File

---
+++ Largest.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+
+    int a,b,c;
+        printf("enter\n");
+        scanf("%d %d %d",&a,&b,&c);
+    if(a>=b)
+    {
+        if(a>=c)
+        {
+        printf("The largest number is : %d",a);
+        } 
+        else
+        {printf("The largest number is : %d",c);}
+     
+    }
+    
+    else if(b>=a)
+    {
+        if(b>=c)
+        {
+            printf("The largest number is : %d",b);
+        }
+        else
+        {
+                printf("The largest number is : %d",c);
+        }
+        
+    }
+    
+  
+        
+        
+    return 0;
+}
